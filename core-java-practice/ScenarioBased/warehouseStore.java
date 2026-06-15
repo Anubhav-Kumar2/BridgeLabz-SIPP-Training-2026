29552829a9bf7a323e78b8975a700c9390285943
Anubhav-Kumar2 <anubhavkatiyar9876@gmail.com>
2026-06-15 09:40:19 +0530

    Add core java practice files

---
+++ core-java-practice/ScenarioBased/warehouseStore.java
@@ -0,0 +1,5 @@
+public class warehouseStore {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ core-java-practice/practice problem/reverse2ndnumber.java
@@ -0,0 +1,5 @@
+public class reverse2ndnumber {
+    public static void main(String[] args) {
+        
+    }
+}
